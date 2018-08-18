7b768e4c4fe5539ec6b2d6ea28283c116b0b61e6
yukairex <wyktju@gmail.com>
2018-08-18 16:40:20 -0700

    initial upload

---
+++ common.js
@@ -0,0 +1,3 @@
+const CONTRACT_1_ADDRESSES = "0xfea71f436ec963507c26f424d93168e4285cd822"; // the free eth batch transfer
+const explorerTx = "https://etherscan.io/tx/";
+const explorer = "https://etherscan.io/address/"
